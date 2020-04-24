66eed38e0c0f90331e4c95973fb7468b3810e6f4
BrandonCalderon77 <64288175+BrandonCalderon77@users.noreply.github.com>
2020-04-24 18:14:13 -0500

    Add files via upload

---
+++ js/archivo_java.js
@@ -0,0 +1,19 @@
+//  $(".siderbar_menu_bar").on('click',function(){
+//      $('.siderbar_contenido').toggleClass('.abrir')
+//  }
+//  )
+
+
+$(".siderbar_menu_bar").on('click',function(){
+      $('.siderbar_men').toggleClass('abrir')
+}
+)
+
+
+
+
+
+// $(".menu_bar").on('click',function(){
+//     $('.contenido').toggleClass('abrir')
+// }
+// )
